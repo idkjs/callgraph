bf7a51ba0d4e1d6db87eba0de6f81c2928e8779e
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-24 11:40:15 +0200

    Lists any files in rootdir.

--- list_json_files_in_dirs.ml
+++ list_json_files_in_dirs.ml
@@ -2,11 +2,35 @@
 (* Author: Hugues Balp *)
 (* This file generates a directory tree listing all json files of each directory *)
 
+exception Unexpected_Error
+
 (* Anonymous argument *)
 let spec =
   let open Core.Std.Command.Spec in
   empty
   +> anon ("rootdir" %: string)
+
+(* List all json files present in the rootdir and all its subdirectories *)
+let recursive_list_directories (rootdir:string) : unit =
+
+  try
+    (
+      let files : string array = Sys.readdir rootdir in
+      let files = Array.to_list files
+      in
+      List.iter
+	(
+	  fun file -> 
+	    Printf.printf "file: %s\n" file;
+	)
+	files
+    )
+  with
+    Sys_error msg -> 
+      (
+	Printf.printf "list_json_files_in_dirs.ml:ERROR: %s\n" msg;
+	raise Unexpected_Error
+      )
 
 (* Basic command *)
 let usage : string = "Generation of a directory tree listing all json files present in each directory\n"
@@ -20,6 +44,7 @@ let command =
 
 	Printf.printf "Listing json files in rootdir \"%s\" and its subdirectories...\n" rootdir;
 	Printf.printf "--------------------------------------------------------------------------------\n";
+	recursive_list_directories rootdir
     )
 
 (* Running Basic Commands *)
