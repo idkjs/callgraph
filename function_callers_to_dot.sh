6a57665c53a5e517fee665ba80189b678c6ea5f0
Hugues Balp <hugues.balp@thalesgroup.com>
2015-08-30 13:32:37 +0200

    Take into account the different configurations of input parameters.

--- function_callers_to_dot.sh
+++ function_callers_to_dot.sh
@@ -3,17 +3,28 @@
 # author: Hugues Balp
 # example: function_callers_to_dot.native callees "main" "int main()" `pwd`/test_dummy.c
 # with configuration = callers | callees | c2c
-configuration=$1
-entry_point_id=$2
-entry_point_sign=$3
-src_file_absolute_path=$4
+direction=$1
 
+caller_id=$2
+caller_sign=$3
+caller_jsonfile_absolute_path=$4
+
+echo "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
 echo "Generate a dot file following either callers, callees or any path between a caller and a callee"
+echo "file ${src_file_absolute_path}: try to generate a ${direction} dot graph from entry point \"${caller_sign}\""
+echo "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"
 
-echo "################################################################################"
-echo "file ${src_file_absolute_path}: try to generate a ${configuration} dot graph from entry point \"${entry_point_sign}\""
-echo "################################################################################"
-function_callers_to_dot.native "${configuration}" "${entry_point_id}" "${entry_point_sign}" "${src_file_absolute_path}"
+if [ -z ${callee_sign} ]
+then
+    files=$5
+    function_callers_to_dot.native "${direction}" "${caller_id}" "${caller_sign}" "${caller_jsonfile_absolute_path}" $files    
+else    
+    callee_id=$5
+    callee_sign=$6
+    callee_jsonfile_absolute_path=$7
+    files=$8
+    function_callers_to_dot.native "${direction}" "${caller_id}" "${caller_sign}" "${caller_jsonfile_absolute_path}" "${callee_id}" "${callee_sign}" "${callee_jsonfile_absolute_path}" $files
+fi
 if [ $? -ne 0 ]; then
     echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
     echo "ERROR in function_callers_to_dot.native ${src_dirname}/${src_filename} ${defined_symbols}. Stop here !"
