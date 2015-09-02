2794884b3ef3031b9dfb1884727ce52d6a385f93
Hugues Balp <hugues.balp@thalesgroup.com>
2015-09-02 22:18:25 +0200

    Reproduces the error that occurs when trying to parse the whole defined symbols file of the SSO service_provider:
    root@idp:/opt/uc_sso/src/service_provider# parse_json_symbols.native defined_symbols.all.gen.json
    Uncaught exception:
    
      (Failure "Buffer.add: cannot grow buffer")
    
    Raised at file "pervasives.ml", line 30, characters 22-33
    Called from file "buffer.ml", line 66, characters 9-50
    Called from file "buffer.ml", line 83, characters 34-46
    Called from file "src/in_channel.ml", line 41, characters 6-43
    Called from file "src/in_channel.ml", line 45, characters 2-9
    Called from file "src/exn.ml", line 45, characters 8-11
    Re-raised at file "src/exn.ml", line 48, characters 12-15
    Called from file "src/in_channel.ml", line 84, characters 21-49
    Called from file "src/command.ml", line 1877, characters 6-97
    Called from file "src/exn.ml", line 88, characters 6-10
    In_channel read file defined_symbols.all.gen.json...
    root@idp:/opt/uc_sso/src/service_provider#

--- parse_json_symbols.ml
+++ parse_json_symbols.ml
@@ -2,79 +2,101 @@
 (* tangled from ~/org/technology/data/data.org *)
 (* adapted from /media/users/balp/tests/data/interchange/json/test_random_event/test_yojson_read.ml *)
 
-(* open Core.Std *)
+open Core.Std
 
 let read_json_file (filename:string) : Yojson.Basic.json =
 
   Printf.printf "In_channel read file %s...\n" filename;
   (* Read JSON file into an OCaml string *)
-  let buf = Core.Std.In_channel.read_all filename in           
+  let buf = Core.Std.In_channel.read_all filename in
   (* Use the string JSON constructor *)
   let json = Yojson.Basic.from_string buf in
   json
 
-let parse_json_file (content:string) : unit =
+(* let parse_json_file (content:string) : unit = *)
 
-  Printf.printf "atdgen parsed json file is :\n";
-  (* Use the atdgen JSON parser *)
-  let file : Callgraph_t.file = Callgraph_j.file_of_string content in
-  print_endline (Callgraph_j.string_of_file file)
+(*   Printf.printf "atdgen parsed json file is :\n"; *)
+(*   (\* Use the atdgen JSON parser *\) *)
+(*   let file : Callgraph_t.file = Callgraph_j.file_of_string content in *)
+(*   print_endline (Callgraph_j.string_of_file file) *)
 
-let parse_json_dir (content:string) (dirfullpath:string): unit =
+(* let parse_json_dir (content:string) (dirfullpath:string): unit = *)
 
-  Printf.printf "atdgen parsed json directory is :\n";
-  (* Use the atdgen JSON parser *)
-  let dir : Callgraph_t.dir = Callgraph_j.dir_of_string content in
-  print_endline (Callgraph_j.string_of_dir dir);
+(*   Printf.printf "atdgen parsed json directory is :\n"; *)
+(*   (\* Use the atdgen JSON parser *\) *)
+(*   let dir : Callgraph_t.dir = Callgraph_j.dir_of_string content in *)
+(*   print_endline (Callgraph_j.string_of_dir dir); *)
 
-  (* Parse the json files contained in the current directory *)
-  (match dir.files with
-   | None -> ()
-   | Some files -> 
-      List.iter
-	( fun f -> 
+(*   (\* Parse the json files contained in the current directory *\) *)
+(*   (match dir.files with *)
+(*    | None -> () *)
+(*    | Some files ->  *)
+(*       List.iter *)
+(* 	( fun f ->  *)
 	  
-	  (* let jsoname_file = String.concat "" [ f; ".file.callers.json" ] in *)
-	  let dirpath : string = Filename.basename dirfullpath in
-	  let jsoname_file = 
-	    if String.compare dirpath dirfullpath == 0
-	    then f
-	    else String.concat "" [ dirpath; "/"; f ]
-	  in
-	  let json : Yojson.Basic.json = read_json_file jsoname_file in
-	  let content : string = Yojson.Basic.to_string json in
-	  (* Printf.printf "Read %s content is:\n %s: \n" f content; *)
-	  parse_json_file content
-	)
-	files
-  )
+(* 	  (\* let jsoname_file = String.concat "" [ f; ".file.callers.json" ] in *\) *)
+(* 	  let dirpath : string = Filename.basename dirfullpath in *)
+(* 	  let jsoname_file =  *)
+(* 	    if String.compare dirpath dirfullpath == 0 *)
+(* 	    then f *)
+(* 	    else String.concat "" [ dirpath; "/"; f ] *)
+(* 	  in *)
+(* 	  let json : Yojson.Basic.json = read_json_file jsoname_file in *)
+(* 	  let content : string = Yojson.Basic.to_string json in *)
+(* 	  (\* Printf.printf "Read %s content is:\n %s: \n" f content; *\) *)
+(* 	  parse_json_file content *)
+(* 	) *)
+(* 	files *)
+(*   ) *)
+
+(* let parse_json_symbols (content:string) (dirfullpath:string): unit = *)
+
+(*   Printf.printf "atdgen parsed json directory is :\n"; *)
+(*   (\* Use the atdgen JSON parser *\) *)
+(*   let dir : Callgraph_t.dir = Callgraph_j.dir_of_string content in *)
+(*   print_endline (Callgraph_j.string_of_dir dir); *)
+
+(*   (\* Parse the json files contained in the current directory *\) *)
+(*   (match dir.files with *)
+(*    | None -> () *)
+(*    | Some files ->  *)
+(*       List.iter *)
+(* 	( fun f ->  *)
+	  
+(* 	  (\* let jsoname_file = String.concat "" [ f; ".file.callers.json" ] in *\) *)
+(* 	  let dirpath : string = Filename.basename dirfullpath in *)
+(* 	  let jsoname_file =  *)
+(* 	    if String.compare dirpath dirfullpath == 0 *)
+(* 	    then f *)
+(* 	    else String.concat "" [ dirpath; "/"; f ] *)
+(* 	  in *)
+(* 	  let json : Yojson.Basic.json = read_json_file jsoname_file in *)
+(* 	  let content : string = Yojson.Basic.to_string json in *)
+(* 	  (\* Printf.printf "Read %s content is:\n %s: \n" f content; *\) *)
+(* 	  parse_json_file content *)
+(* 	) *)
+(* 	files *)
+(*   ) *)
 
 (* Anonymous argument *)
 let spec =
   let open Core.Std.Command.Spec in
   empty
-  +> anon ("dirname" %: string)
-  +> anon (maybe("jsondirext" %: string))
+  +> anon ("allsymbols_jsonfilepath" %: string)
 
 (* Basic command *)
 let command =
   Core.Std.Command.basic
-    ~summary:"Parses a callers's json directory with the atdgen library"
+    ~summary:"Parses the global list of defined symbols"
     ~readme:(fun () -> "More detailed information")
     spec
     (
-      fun dirfullpath jsondirext () -> 
+      fun allsymbols_jsonfilepath () -> 
 
-	let jsoname_dir : string = 
-	  (match jsondirext with
-	  | None -> Printf.sprintf "%s" dirfullpath
-	  | Some dirext -> Printf.sprintf "%s.%s" dirfullpath dirext
-	  )
-	in
-	let json : Yojson.Basic.json = read_json_file jsoname_dir in
+	let json : Yojson.Basic.json = read_json_file allsymbols_jsonfilepath in
 	let content : string = Yojson.Basic.to_string json in
-	(* Printf.printf "Read directory content is:\n %s: \n" content; *)
-	parse_json_dir content dirfullpath
+	Printf.printf "Read defined symbols content is:\n %s: \n" content
+	(* parse_json_symbols content dirfullpath *)
     )
 
 (* Running Basic Commands *)
@@ -83,6 +105,6 @@ let () =
 
 (* Local Variables: *)
 (* mode: tuareg *)
-(* compile-command: "ocamlbuild -use-ocamlfind -package atdgen -package core -package yojson -tag thread parse_json_dir.native" *)
-(* compile-command: "ocamlbuild -use-ocamlfind -package atdgen -package core -tag thread parse_json_dir.native" *)
+(* compile-command: "ocamlbuild -use-ocamlfind -package atdgen -package core -package yojson -tag thread parse_json_symbols.native" *)
+(* compile-command: "ocamlbuild -use-ocamlfind -package atdgen -package core -tag thread parse_json_symbols.native" *)
 (* End: *)
