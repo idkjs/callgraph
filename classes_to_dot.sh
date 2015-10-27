2e6db6fb5e21865c3cd330eefd5f4bdf45105d6c
Hugues Balp <hugues.balp@thalesgroup.com>
2015-10-27 21:00:40 +0100

    Replaced "function_calls_to_dot" by "classes_to_dot" Replaced "function_callers" by "base_classes" Replaced "gfct_callers" by "gbase_classes" Replaced "function_callees" by "child_classes" Replaced "gfct_callees" by "gchild_classes" Replaced "function_c2c" by "child_2_base_class"

--- classes_to_dot.sh
+++ classes_to_dot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -x
 # author: Hugues Balp
-# example: function_calls_to_dot.native callees "main" "int main()" `pwd`/test_dummy.c
+# example: classes_to_dot.native callees "main" "int main()" `pwd`/test_dummy.c
 # with configuration = callers | callees | c2c
 direction=$1
 caller_jsonfile_absolute_path=$2
@@ -21,17 +21,17 @@ echo "DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 if [ -z $extra_args ]
 then
     files=$1
-    function_calls_to_dot.native "${direction}" "${caller_jsonfile_absolute_path}" "${caller_id}" "${caller_sign}" $files    
-else    
+    classes_to_dot.native "${direction}" "${caller_jsonfile_absolute_path}" "${caller_id}" "${caller_sign}" $files    
+else
     callee_jsonfile_absolute_path=$1
     callee_id=$2
     callee_sign=$3
     files=$4
-    function_calls_to_dot.native "${direction}" "${caller_jsonfile_absolute_path}" "${caller_id}" "${caller_sign}" "${callee_jsonfile_absolute_path}" "${callee_id}" "${callee_sign}"  $files
+    classes_to_dot.native "${direction}" "${caller_jsonfile_absolute_path}" "${caller_id}" "${caller_sign}" "${callee_jsonfile_absolute_path}" "${callee_id}" "${callee_sign}"  $files
 fi
 if [ $? -ne 0 ]; then
     echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
-    echo "ERROR in function_calls_to_dot.native ${src_dirname}/${src_filename} ${defined_symbols}. Stop here !"
+    echo "ERROR in classes_to_dot.native ${src_dirname}/${src_filename} ${defined_symbols}. Stop here !"
     echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
     exit -1
 fi
