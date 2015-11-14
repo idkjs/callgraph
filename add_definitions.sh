cb92b61687ca145d8f0435b689edf4eccbb417fe
Hugues Balp <hugues.balp@thalesgroup.com>
2015-11-14 16:17:11 +0100

    Add a new OCAML backend to complete function & methods declarations with refereances to their definitions/implementations

--- add_definitions.sh
+++ add_definitions.sh
@@ -4,23 +4,24 @@
 # WARNING: We assume here that two input parameters are present and correspond to:
 # param1: a valid json file
 # param2: a valid path to the defined symbol json file
+# This file has been adapted from file add_definitions.ml
 rootdir_fullpath=$1
 searchdirs_fullpath=$2
 
-echo "Try to complete definitions with declarations when possible in files *.file.callers.gen.json present in root directory \"${rootdir_fullpath}\""
-echo "function definitions can be found either in root directory or in search directories: \"$searchdirs_fullpath\""
+echo "Try to complete declarations with definitions when possible in files *.file.callers.gen.json present in root directory \"${rootdir_fullpath}\""
+echo "function declarations can be found either in root directory or in search directories: \"$searchdirs_fullpath\""
 
 for json in `find $rootdir_fullpath -name "*.file.callers.gen.json"`
 do
 src_dirname=`dirname ${json}`
 src_filename=`basename ${json} ".file.callers.gen.json"`
 echo "################################################################################"
-echo "file ${src_dirname}/${src_filename}: try to add declarations to metadata file ${json}"
+echo "file ${src_dirname}/${src_filename}: try to add definitions to function declaration's metadata file ${json}"
 echo "################################################################################"
 add_declarations.native ${src_dirname}/${src_filename} ${rootdir_fullpath} ${searchdirs_fullpath}
 if [ $? -ne 0 ]; then
     echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
-    echo "ERROR in add_declarations.native ${src_dirname}/${src_filename} ${rootdir_fullpath}. Stop here !"
+    echo "ERROR in add_definitions.native ${src_dirname}/${src_filename} ${rootdir_fullpath}. Stop here !"
     echo "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE"
     exit -1
 fi
