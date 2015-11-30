606854bd548e39fb80293c738af46073f2f7d5ec
Hugues Balp <hugues.balp@thalesgroup.com>
2015-11-30 08:41:15 +0100

    Added a configuration file

---
+++ config.ml
@@ -0,0 +1,24 @@
+(******************************************************************************)
+(*   Copyright (C) 2014-2015 THALES Communication & Security                  *)
+(*   All Rights Reserved                                                      *)
+(*   KTD SCIS 2014-2015                                                       *)
+(*   Use Case Legacy TOSA                                                     *)
+(*   author: Hugues Balp                                                      *)
+(*                                                                            *)
+(******************************************************************************)
+
+let get_type_dir () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//dir";;
+
+let get_type_dir_symbols () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//dir_symbols";;
+
+let get_type_file () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//file";;
+
+let get_type_record () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//record";;
+
+let get_type_fct_decl () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//fct_decl";;
+
+let get_type_fct_def () : string = "http://com.thalesgroup.fr/tcs/callers/1.0.0#//fct_def";;
+
+(* Local Variables: *)
+(* mode: tuareg *)
+(* End: *)
